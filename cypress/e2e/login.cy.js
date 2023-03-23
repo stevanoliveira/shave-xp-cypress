b20cb30e94fbe708d19dc0b5febdd40253b22c5e
Stevan Oliveira <stevan.oliver@hotmail.com>
2023-03-23 13:29:29 -0300

    criação de task, comando personalizado e fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,83 +4,108 @@ import loginPage from "../support/pages/login"
 import shaversPage from "../support/pages/shavers"
 import header from "../support/components/header"
 
+import data from "../fixtures/users.json"
+
 describe("login", () => {
-  context("quando submeto o formulário", () => {
-    it("deve logar com sucesso", () => {
-      const user = {
-        name: "stevan",
-        email: "stevan@gmail.com",
-        password: "senha123",
-      }
+	/**
+	 * MODO 3: importando no beforeEach
+	 * beforeEach(() => {
+	 *   cy.fixture("user-login").then(function (data) {
+	 *     this.data = data
+	 *   })
+	 * })
+	 */
 
-      loginPage.submit(user.email, user.password)
-      shaversPage.header.userShouldLoggedIn(user.name)
-    })
+	context("quando submeto o formulário", () => {
+		it.only("deve logar com sucesso", () => {
+			/**
+			 * MODO 1: usando o methodo cy.fixture()
+			 * cy.fixture("user-login").then(function (data) {
+			 *   loginPage.submit(data.email, data.password)
+			 *   shaversPage.header.userShouldLoggedIn(data.name)
+			 *   loginPage.submit(data.email, data.password)
+			 *   shaversPage.header.userShouldLoggedIn(data.name)
+			 * })
+			 */
 
-    it("não deve logar com senha incorreta", () => {
-      const user = {
-        name: "stevan",
-        email: "stevan@gmail.com",
-        password: "wrongPassword",
-      }
+			/**
+			 * MODO 2: importa a fixture direto no código, via JS puro
+			 * */
 
-      loginPage.submit(user.email, user.password)
+			// dado que eu tenho um NOVO usuário cadastrado
+			const user = data.success
 
-      const message =
-        "Ocorreu um erro ao fazer login, verifique suas credenciais."
+			cy.createUser(user)
 
-      loginPage.noticeShouldBe(message)
-    })
+			loginPage.submit(user.email, user.password)
 
-    it("não deve logar com email incorreta", () => {
-      const user = {
-        name: "stevan",
-        email: "inexistentemail@gmail.com",
-        password: "inexistentpass",
-      }
+			shaversPage.header.userShouldLoggedIn(user.name)
 
-      loginPage.submit(user.email, user.password)
+			/**
+			 *
+			 * MODO 3:importando no beforeEach
+			 * obs. Para que esse modo funcione, o 'it' deve usar função
+			 * convencional function() {} ao invés de () => {}
+			 * const user = this.data
+			 * loginPage.submit(user.email, user.password)
+			 * shaversPage.header.userShouldLoggedIn(user.name)
+			 */
+		})
 
-      const message =
-        "Ocorreu um erro ao fazer login, verifique suas credenciais."
+		it("não deve logar com senha incorreta", () => {
+			const user = data.invypass
 
-      loginPage.noticeShouldBe(message)
-    })
+			loginPage.submit(user.email, user.password)
 
-    it("campos obrigatórios", () => {
-      loginPage.submit()
-      loginPage.requiredFields("E-mail é obrigatório", "Senha é obrigatória")
-    })
-  })
+			const message = "Ocorreu um erro ao fazer login, verifique suas credenciais."
 
-  context("senha muito curta", () => {
-    const passwords = ["1", "12", "123", "1234", "12345"]
+			loginPage.noticeShouldBe(message)
+		})
 
-    passwords.forEach((p) => {
-      it(`não deve logar com a senha: ${p}`, () => {
-        loginPage.submit("stevan@gmail.com", p)
-        loginPage.alertShouldBe("Pelo menos 6 caracteres")
-      })
-    })
-  })
+		it("não deve logar com email incorreta", () => {
+			const user = data.email404
 
-  context("email no formato incorreto", () => {
-    const emails = [
-      "stevan$gmail.com",
-      "stevan.com.br",
-      "@gmail.com",
-      "@",
-      "stevan@",
-      "121212",
-      "xpto123",
-      "@#$%¨&*",
-    ]
+			loginPage.submit(user.email, user.password)
 
-    emails.forEach((e) => {
-      it(`não deve logar com a email: ${e}`, () => {
-        loginPage.submit(e, "senha123")
-        loginPage.alertShouldBe("Informe um email válido")
-      })
-    })
-  })
+			const message = "Ocorreu um erro ao fazer login, verifique suas credenciais."
+
+			loginPage.noticeShouldBe(message)
+		})
+
+		it("campos obrigatórios", () => {
+			loginPage.submit()
+			loginPage.requiredFields("E-mail é obrigatório", "Senha é obrigatória")
+		})
+	})
+
+	context("senha muito curta", () => {
+		// const passwords = ["1", "12", "123", "1234", "12345"]
+
+		data.shortpass.forEach((p) => {
+			it(`não deve logar com a senha: ${p}`, () => {
+				loginPage.submit("stevan@gmail.com", p)
+				loginPage.alertShouldBe("Pelo menos 6 caracteres")
+			})
+		})
+	})
+
+	context("email no formato incorreto", () => {
+		// const emails = [
+		//   "stevan$gmail.com",
+		//   "stevan.com.br",
+		//   "@gmail.com",
+		//   "@",
+		//   "stevan@",
+		//   "121212",
+		//   "xpto123",
+		//   "@#$%¨&*",
+		// ]
+
+		data.invemails.forEach((e) => {
+			it(`não deve logar com a email: ${e}`, () => {
+				loginPage.submit(e, "senha123")
+				loginPage.alertShouldBe("Informe um email válido")
+			})
+		})
+	})
 })
